96645edfeeec011f1d4346d84bcb76744a45378d
冷石Boy <coldstoneboy@qq.com>
2026-09-20 07:52:03 +0800

    feat(prelook): add multiple community sites as default start urls
    新增贴吧、虎扑、小黑盒和NGA论坛作为默认启动页面，丰富初始访问站点选择

--- apps/prelook/wxt.config.ts
+++ apps/prelook/wxt.config.ts
@@ -52,7 +52,12 @@ export default defineConfig({
       doubao: "C:\\Users\\coldstonestudio\\AppData\\Local\\Doubao\\Application\\app\\Doubao.exe",
       firefox: ZEN_BINARY,
     },
-    startUrls: ["https://bbs.hupu.com/4860"],
+    startUrls: [
+      "https://tieba.baidu.com/",
+      "https://bbs.hupu.com/4860",
+      "https://www.xiaoheihe.cn/app/bbs/home/",
+      "https://ngabbs.com/thread.php?fid=-152678",
+    ],
   },
   autoIcons: {
     baseIconPath: "./assets/logo.webp",
