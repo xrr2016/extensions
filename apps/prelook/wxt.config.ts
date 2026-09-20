97ff2950c2ca1fa1872f79083dcb68e63e512160
冷石Boy <coldstoneboy@qq.com>
2026-09-20 08:49:44 +0800

    fix(prelook): 修复火狐扩展id格式问题
    为符合火狐扩展manifest要求，为扩展id添加大括号包裹

--- apps/prelook/wxt.config.ts
+++ apps/prelook/wxt.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     // collect any user data, so the list is empty.
     browser_specific_settings: {
       gecko: {
-        id: "f256e499-1229-4224-a321-de38e0dc081d",
+        id: "{f256e499-1229-4224-a321-de38e0dc081d}",
         data_collection_permissions: {
           required: ["none"],
         },
